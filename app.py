c38ed7fda721d8c94a79ea5e8d146b071616d219
emmanuel cortes <emnlc.dev@gmail.com>
2024-11-23 18:35:27 -0700

    README updated

--- app.py
+++ app.py
@@ -25,6 +25,6 @@ if __name__ == "__main__":
     
     # Run the Flask app
     try:
-        app.run(debug=True, host='0.0.0.0')
+        app.run(host='0.0.0.0', port=8080)
     except (KeyboardInterrupt, SystemExit):
         scheduler.shutdown()
