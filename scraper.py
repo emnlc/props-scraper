4ba2e853960cca009c9e20669e0ef95720a6908b
emmanuel cortes <emnlc.dev@gmail.com>
2024-12-02 20:04:43 -0700

    Browser options added

--- scraper.py
+++ scraper.py
@@ -34,10 +34,10 @@ async def scraper():
     GAME_LINES = defaultdict(lambda: defaultdict(lambda: defaultdict(dict)))
     
     options = webdriver.ChromeOptions()
-    # options.add_argument("--headless=new")
-    # options.add_argument("--disable-gpu")
-    # options.add_argument("--no-sandbox")
-    # options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--headless=new")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     
     async with webdriver.Chrome(options=options) as browser:
         print("getting ready to scrape. . .")
